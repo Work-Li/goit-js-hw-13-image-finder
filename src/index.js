93169e4e17092748521e000779a9fc978492911f
Liliia Bakalinska <lili.lomaka@gmail.com>
2021-11-15 02:04:14 +0200

    add openBigImage

--- src/index.js
+++ src/index.js
@@ -3,44 +3,65 @@ import cardTemplate from './js/templates/card.hbs';
 import ApiSrevice from './js/apiService.js';
 import '@pnotify/core/dist/BrightTheme.css';
 import { error } from '@pnotify/core';
+import * as basicLightbox from 'basiclightbox';
+
+
 
 const searchFormEl = document.querySelector('#search-form');
 const loadBtnEl = document.querySelector('.btn-load-more');
 const galleryEl = document.querySelector('.gallery');
-const btnUpEl = document.querySelector('.btn-up')
+const btnUpEl = document.querySelector('.btn-up');
+
 
 const imagesApiService = new ApiSrevice();
 
 
 searchFormEl.addEventListener('submit', onSearch);
 loadBtnEl.addEventListener('click', onLoadMore);
+galleryEl.addEventListener('click', openBigImage);
 
 function onSearch(event) {
     event.preventDefault();
-    
+    // clearGallery();
     imagesApiService.query = event.currentTarget.query.value;
     imagesApiService.resetPage();
     
     if (!imagesApiService.query) {
         onError('Please enter a correct request');
-
+        hideLoadMoreBtn();
         return;
     }
-    imagesApiService.fetchImages()
-    .then(images => {
-        clearGallery();
-        appendImagesMarkup(images);
-    })
-    .catch(onError('Sorry, please repeat request')); 
+    imagesApiService
+        .fetchImages()
+        .then(images => {
+            clearGallery();
+            if (!images.length) {
+                hideLoadMoreBtn();
+                onError('No images for this request');}
+            appendImagesMarkup(images);
+        })
+        .catch(error => {
+            onError('Sorry, please repeat request')}
+        ); 
     addBtnUp(); 
     addLoadMoreBtn();    
 }
 
 function onLoadMore() {
-    imagesApiService.fetchImages().
-        then(images => {
-            // console.log(images);
+    imagesApiService
+        .fetchImages()
+        .then(images => {
+            if(images.length === 0) {
+                hideLoadMoreBtn();
+                onError("No more images");
+            }
+            loadBtnEl.classList.remove('disabled');
             appendImagesMarkup(images)});
+    loadBtnEl.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+        });
+
 }
 
 function appendImagesMarkup (images) {
@@ -82,4 +103,15 @@ function onError(message) {
      icon: null
     
     });
+}
+
+function openBigImage(event) {
+    const instance = basicLightbox.create(`
+  <img src=${event.target.dataset.src} width="600" height="400">
+`);
+
+ if (event.target.nodeName !== 'IMG') {
+     return;
+ }
+    instance.show();
 }
